6ba4c2b2108e90d3a3be5cf058f4dc8dac585c8b
Chameli Dilshani <chameli.d@eyepax.com>
2022-05-03 10:53:37 +0530

    Callback handlers in JSX

--- src/App.js
+++ src/App.js
@@ -5,27 +5,24 @@ const App=( )=> {
   // do something in between
   const stories = [
     {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
     },
     {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
     },
-    ];
-  
+  ];
     
-  const handleChange=event=>{
-    console.log(event.target.value)
-  }
+ 
 
   const List=props =>
   props.list.map(item=>(
@@ -37,13 +34,28 @@ const App=( )=> {
     </div>
   )
     )
+  const Search=()=>{
+    const [searchTerm,setSearchTerm]=React.useState('');
+  
+    const handleChange=event=>{
+      setSearchTerm(event.target.value)
+     }
+     return(
+       <div>
+         <lable htmlFor='search'>Seaarch</lable>
+      <input id="search" type="text" onChange={handleChange}/>
+
+      <p>Searching for <strong>{searchTerm}</strong></p>
+       </div>
+     )
+  }
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <lable htmlFor='search'>Seaarch</lable>
-      <input id="search" type="text" onClick={handleChange}/>
+      
+      <Search/>
       <hr/>
-      <List list={stories}/>
+     <List list={stories}/>
      </div>
   )
 };
