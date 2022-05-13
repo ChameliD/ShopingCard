bb0d3f14afa06d66634171b73bb25fff8bf02d43
Chameli Dilshani <chameli.d@eyepax.com>
2022-05-13 12:14:12 +0530

    JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Greeding from './Components/Greeding';
+import Hello from './Components/Hello';
 import Welcome from './Components/Welcome';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Greeding/>
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
