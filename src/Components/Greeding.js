bab65ca5ca024a8fbf75ca9a25fd040cbd1cf53a
Chameli <Chameli@example.com>
2022-05-17 09:11:52 +0530

    destructuring props and sates

--- src/Components/Greeding.js
+++ src/Components/Greeding.js
@@ -2,11 +2,21 @@
 
 import React from 'react'
 
-const Greeding=({name,num,children})=>{
+const Greeding= props=>{
+  const{name,num,children}=props
 return(
   <div>
     <h1>Hello {name}, Your index number is {num}</h1>
     {children}
 </div>
   )}
+
+/*const Greeding=({name,num,children})=>{
+return(
+  <div>
+    <h1>Hello {name}, Your index number is {num}</h1>
+    {children}
+</div>
+  )}*/
+
 export default Greeding
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -4,8 +4,17 @@ import React,{Component} from "react";
 
 class Welcome extends Component{
     render(){
-        return <h1> Welcome {this.props.name} your number is {this.props.num}</h1> 
+        const{name,num}=this.props
+        //const{state1,state2}=this.props
+        return <h1> Welcome {name} your number is {num}</h1> 
     }
 }
+
+
+/*class Welcome extends Component{
+    render(){
+        return <h1> Welcome {this.props.name} your number is {this.props.num}</h1> 
+    }
+}*/
 
 export default Welcome
