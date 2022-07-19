383ae398cfb8c6e6a5857c75d73feda336592a7b
Chameli <Chameli@example.com>
2022-07-19 16:43:18 +0530

    install react

--- src/Components/Subject.js
+++ src/Components/Subject.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Subject() {
   return (
-    <div><h1>Subject Name : {subject.name}<br/>Number of creadits : {subject.creadit}</h1>
-    </div>
+    /*<div><h1>Subject Name : {subject.name}<br/>Number of creadits : {subject.creadit}</h1>
+  </div>*/
+  <div>hi</div>
   )
 }
 
